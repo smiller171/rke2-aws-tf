c6f96a58a7acb4ddb611c62dca154c2a6b634bde
Scott Miller <scott@millergeek.xyz>
2022-12-22 11:03:43 -0500

    remove deprecated template module

--- modules/userdata/outputs.tf
+++ modules/userdata/outputs.tf
@@ -1,14 +1,17 @@
 output "templated" {
-  value = templatefile("${path.module}/files/rke2-init.sh", {
-    type = var.agent ? "agent" : "server"
+  value = templatefile(
+    "${path.module}/files/rke2-init.sh",
+    {
+      type = var.agent ? "agent" : "server"
 
-    server_url   = var.server_url
-    token_bucket = var.token_bucket
-    token_object = var.token_object
-    config       = var.config
-    ccm          = var.ccm
+      server_url   = var.server_url
+      token_bucket = var.token_bucket
+      token_object = var.token_object
+      config       = var.config
+      ccm          = var.ccm
 
-    pre_userdata  = var.pre_userdata
-    post_userdata = var.post_userdata
-  })
+      pre_userdata  = var.pre_userdata
+      post_userdata = var.post_userdata
+    }
+  )
 }
